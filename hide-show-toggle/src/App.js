1d131eeaa82f34a8cd45ccc41d343a0a69b3f0fa
Samim Aktar <samimaktar0773@gmail.com>
2021-11-06 22:23:13 +0530

    Done

--- hide-show-toggle/src/App.js
+++ hide-show-toggle/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {status ? <h1>Hello Programmer</h1> : null}
+
+      <button onClick={()=>setStatus(false)}>Hide</button>
+      <button onClick={()=>setStatus(true)}>Show</button>
+
+      <button onClick={()=>setStatus(!status)}>Toggle</button>
     </div>
   );
 }
