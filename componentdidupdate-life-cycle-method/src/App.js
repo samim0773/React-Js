28070c0da448fbfda1f3e6e7ccd59a9fa0fc1853
Samim Aktar <samimaktar0773@gmail.com>
2021-11-08 01:03:16 +0530

    update

--- componentdidupdate-life-cycle-method/src/App.js
+++ componentdidupdate-life-cycle-method/src/App.js
@@ -7,18 +7,32 @@ class App extends React.Component {
     super();
     console.log("costructor");
     this.state = {
-      count: 0
+      count: 0,
     };
   }
-  componentDidUpdate(preProps,preState,snapshot) {
-    console.warn("Component did update",preProps);
+  componentDidUpdate(preProps, preState, snapshot) {
+    // console.warn("Component did update",preProps);
+    if(this.state.count < 10){
+      this.setState({count:this.state.count+1})
+    }
   }
   render() {
     console.log("render");
     return (
       <div className="App">
         <h1>component did update {this.state.count}</h1>
-        <button onClick={() =>{this.setState({count:this.state.count+1})}}>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Update Name
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ count: 1 });
+          }}
+        >
           Update Name
         </button>
       </div>
