723a967f688cfe08cfa70b822d0b97a511dceb77
Samim Aktar <samimaktar0773@gmail.com>
2021-11-08 12:36:58 +0530

    initial commite

---
+++ shouldcomponentupdate-life-cycle-method/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate", this.state.count);
+    if (this.state.count > 5 && this.state.count < 10) {
+      return true;
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Should Component Update life cycle method {this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          {" "}
+          Update Count
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
